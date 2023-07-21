3e060bb0a6784e31e95013590a893746b272af3c
fourdotdash <arocena.elijahluis@gmail.com>
2023-07-21 16:37:27 +0800

    fix 1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import "../styles/style.css";
 import "../styles/modern-normalize.css";
 import "../styles/highlight.css";
+import "./first-highlight";
+import "./second-highlight";
+import "./third-highlight";
+import "./fourth-highlight";
